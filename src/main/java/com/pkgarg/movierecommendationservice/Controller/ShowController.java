e8b2a8f5c8c139fdb0fa3d5865cdcb0e612fa5e3
pramodgarg <pramod.garg.pkg@gmail.com>
2022-04-18 10:11:41 +0530

    modified show controller

--- src/main/java/com/pkgarg/movierecommendationservice/Controller/ShowController.java
+++ src/main/java/com/pkgarg/movierecommendationservice/Controller/ShowController.java
@@ -2,6 +2,7 @@ package com.pkgarg.movierecommendationservice.Controller;
 
 import com.pkgarg.movierecommendationservice.Service.IShowService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,22 +26,23 @@ public class ShowController {
     }
 
     @GetMapping("/allShows")
-    public ResponseEntity<List<Object>> getAllShowsByQuery(@RequestParam String query) {
+    public ResponseEntity<?> getAllShowsByQuery(@RequestParam String query) {
         try {
             return ResponseEntity.ok(showService.getAllShowsByQuery(query));
         } catch (Exception e) {
             e.printStackTrace();
-            return (ResponseEntity<List<Object>>) ResponseEntity.notFound();
+            return (ResponseEntity<?>) ResponseEntity.notFound();
         }
     }
 
     @GetMapping("/{userId}/recommendation")
-    public ResponseEntity<Object> getShowRecommendationByQuery(@PathVariable String userId, @RequestParam String query) {
+    public ResponseEntity<?> getShowRecommendationByQuery(@PathVariable String userId, @RequestParam String query) {
         try {
             return ResponseEntity.ok(showService.getNewRecommendation(userId, query));
         } catch (Exception e) {
             e.printStackTrace();
-            return (ResponseEntity<Object>) ResponseEntity.notFound();
+            return (ResponseEntity<?>) ResponseEntity.notFound();
         }
     }
+
 }
